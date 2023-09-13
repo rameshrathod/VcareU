81b7367e304ae9527cc16ab9ec9e4b6ccc7f9c20
“rameshrathod” <rameshrathod1194@gmail.com>
2023-09-13 18:10:48 +0530

    fist commit

---
+++ src/main/java/com/VcareU/VcareUApplication.java
@@ -0,0 +1,21 @@
+package com.VcareU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class VcareUApplication {
+
+	@GetMapping("/get/name")
+	public String getName(){
+		return "Ramesh";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(VcareUApplication.class, args);
+	}
+
+}
