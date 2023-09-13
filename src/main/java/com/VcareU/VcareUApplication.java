0a72694e593babaa1b19575834d262f3ce21357b
rameshrathod <rameshrathod194@gmail.com>
2023-09-13 18:45:48 +0530

    check auto deploy

--- src/main/java/com/VcareU/VcareUApplication.java
+++ src/main/java/com/VcareU/VcareUApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VcareUApplication {
 
-	@GetMapping("/get/name")
+	@GetMapping("/get/name") 
 	public String getName(){
 		return "Ramesh";
 	}
